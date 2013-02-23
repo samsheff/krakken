c39348e1123d385e3520f7786d501d46ea459e44
Sam Sheffres <ssheffres@gmail.com>
2013-02-22 22:33:32 -0800

    Better in-code documentation

--- examples/rw.rb
+++ examples/rw.rb
@@ -1,12 +1,18 @@
+# require Krakken
 require '../lib/krakken.rb'
 
+# Define our Config File
 config = { "ip_address" => "127.0.0.1", "port" => "4568", "launch_code" => "5658" }
 
+# Create a new Krakken Object
 settings = Krakken.new
 
+# Write the config file to disk
 settings.write(config)
 
+# Parse the config file we just saved
 result = settings.parse
 
+# And do something with it
 puts "The server will run on #{result["ip_address"]}:#{result["port"]}"
 puts "The launch code is #{result["launch_code"]}."
--- lib/krakken.rb
+++ lib/krakken.rb
@@ -1,5 +1,6 @@
 class Krakken
 
+	# The method used to parse the config files
 	def parse(config="settings.conf")
 
 		values = []
@@ -8,20 +9,28 @@ class Krakken
 		File.open(config, 'r') do |configfile|
 			# Keep parsing long as there is another unparsed line in the file
 			while (nextline = configfile.gets)
+				# Check to see if we are parsing a comment
 				if nextline[0] != "#"
+					# Remove the whitespace
 					nextline = nextline.gsub(/\s+/, '')
+					# Split the key from the value
+					# And add each to an array
 					splitvalues = nextline.split("=")
 					splitvalues.each {|object| values << object}
 				end
 			end
 		end
+		# Return the parsed values as a hash
 		return Hash[*values.flatten]
 	end
 
+	# The method used to save the config file
 	def write(settings, output="settings.conf")
 
+		# Create the config file as a string to be written to disk
 		config = settings.map{|k,v| "#{k} = #{v}\n"}.join('')
 
+		# And save
 		File.open(output, 'w') {|f| f.write(config)}
 
 	end
